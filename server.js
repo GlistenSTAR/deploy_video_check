ac4bd478f2679c0db54acf4f0d8a690a1de215bd
UY Scuti <75786284+GlistenSTAR@users.noreply.github.com>
2023-11-17 07:48:59 -0800

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/leader', require('./routes/api/leader'));
 app.use('/api/curator', require('./routes/api/curator'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
